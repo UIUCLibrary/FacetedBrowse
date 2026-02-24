e1e64ae8c9a0a2b0ff2a0b60ae472e857e63907f
Alex Dryden <adryden3@illinois.edu>
2026-02-24 14:24:34 -0500

    fix: add missing break to prevent single-list case fallthrough

--- asset/js/facet-render/value.js
+++ asset/js/facet-render/value.js
@@ -40,6 +40,7 @@ const handleUserInteraction = function(thisValue) {
         case 'single_list':
             facet.find('.value').not(thisValue).removeClass('selected');
             thisValue.prop('checked', !thisValue.hasClass('selected'));
+            break
         case 'multiple_list':
             thisValue.toggleClass('selected');
             break;
